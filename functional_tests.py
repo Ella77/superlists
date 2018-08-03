b46d22f752c71d493623966cd05b9dddb7a82176
Ella77 <ella@mykoon.com>
2018-08-03 11:31:40 +0900

    unittest with comment

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 
 from selenium import webdriver
-browser = webdriver.Firefox(executable_path='/Users/soojin/Downloads/geckodriver')
-browser.get('http://localhost:8000')
-assert 'To-Do' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the Test!')
+    if __name__ =='__main__':
+        unittest.main(warnings='ignore')
+#
+# browser = webdriver.Firefox(executable_path='/Users/soojin/Downloads/geckodriver')
+# browser.get('http://localhost:8000')
+# assert 'To-Do' in browser.title
 
